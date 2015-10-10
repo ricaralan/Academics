40cc8914931ec0e97e963e35dd306345b201c781
Alan <richard.alan.oli@gmail.com>
2015-10-10 11:42:01 -0500

    I moved old structure to connect database and queries with controller to folder old. Also I create UserModel with method getUserIfExist for get user by email and password

---
+++ database/models/UserModel.js
@@ -0,0 +1,36 @@
+/**
+*	UserModel
+*	@author Alan Olivares
+*	@version 0.0.1
+*/
+var UserModel = function() {
+
+	var self = this;
+
+	self.table = "academics_user";
+
+	self.getUserIfExist = function(email, password, done) {
+		self.getDateConnection.doQuery(
+			self.getDateConnection.getR()
+			.table(self.table)
+			.filter({
+				user_email : email,
+				user_password : password
+			}).limit(1)
+			, function(err, cursor) {
+				try {
+					cursor.toArray(function(err, users) {
+						done(err, (users.length == 1) ? users[0] : null);
+					});
+				} catch(e) {
+					done(e);
+				}
+			});
+	};
+
+}
+
+// UserModel extends of AbstractModel
+UserModel.prototype = require("./AbstractModel");
+
+module.exports = new UserModel();
