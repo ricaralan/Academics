b61717cca01a4e765e20b02aa14887c7d901b85e
Alan <richard.alan.oli@gmail.com>
2015-10-11 05:46:34 -0500

    I added service util for utilities

---
+++ public/javascripts/angular/services/util.js
@@ -0,0 +1,28 @@
+AcademicsModule.service("util", ["$http", function($http) {
+
+  this.isEmpty = function(value) {
+    return !value || value.length == 0;
+  };
+
+  this.isNumber = function(strNumber) {
+    return !isNaN(parseFloat(strNumber)) && isFinite(strNumber);
+  };
+
+  this.getDate = function(separator) {
+    date = new Date();
+    año = date.getFullYear();
+    mes = date.getMonth() + 1;
+    dia = date.getDate();
+    return año+separator+mes+separator+dia;
+  };
+
+  this.getHour = function() {
+    date = new Date().toString().split(":");
+    return date[0].substr(date[0].length-2,2) + ":" + date[1] + ":" + date[2].substr(0,2);
+  };
+
+  this.getPermisosUserLog = function(callback) {
+    $http.get("/permisos/getPermisosUserLog").success(callback);
+  };
+
+}]);
