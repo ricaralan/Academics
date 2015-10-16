6d64b3d0c71fb90e189823a67e689a3cbcb4b0e6
Alan <richard.alan.oli@gmail.com>
2015-10-15 20:46:09 -0500

    I deleted old angular files

--- public/javascripts/angular/controllers/CoursesController.js
+++ public/javascripts/angular/controllers/CoursesController.js
@@ -1,6 +1,15 @@
 AcademicsModule.controller("CoursesController", ["$scope", "$http", "$routeParams", "$location", function($scope, $http, $routeParams, $location) {
 
+	$scope.getLanguajes = function() {
+		
+	};
 
-	
+	$scope.getCategories = function() {
+
+	};
+
+	$scope.getSubCategories = function() {
+		
+	};
 
 }]);
