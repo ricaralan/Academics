3036c668cbd7afb0af4bf9586bf004841952439b
Alan <richard.alan.oli@gmail.com>
2015-10-16 06:03:12 -0500

    I added basic structure model

---
+++ database/models/__TableModelBasicStructure.js
@@ -0,0 +1,17 @@
+/**
+*	__TableModelBasicStructure
+*	@author Alan Olivares
+*	@version 0.0.1
+*/
+var __TableModelBasicStructure = function() {
+
+	var self = this;
+
+	self.table = "";
+
+}
+
+// __TableModelBasicStructure extends of AbstractModel
+__TableModelBasicStructure.prototype = require("./AbstractModel");
+
+module.exports = new __TableModelBasicStructure();
