4e8c806a1b10434ed997f7c667c2d7c76710fd6d
Alan <richard.alan.oli@gmail.com>
2015-10-16 06:43:45 -0500

    I deleted old structure and modified new structure

--- routes/index/index.js
+++ routes/index/index.js
@@ -11,7 +11,7 @@ router.get("/", function (req, res) {
 		});
 	} else {
 		// Hay un usuario logueado
-		res.render("academics_views", {
+		res.render("index", {
 		    title : "Academics",
 		    user  : req.user
 		  });
@@ -26,7 +26,7 @@ router.get("/login", function (req, res) {
 		});
 	} else {
 		// Hay un usuario logueado
-		res.render("academics_views", {
+		res.render("index", {
 		    title : "Academics",
 		    user  : req.user
 		  });
