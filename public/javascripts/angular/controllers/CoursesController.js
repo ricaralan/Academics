2d48645d066518d84f8355b768d1bcb6447f2f75
Alan <richard.alan.oli@gmail.com>
2015-10-16 05:20:58 -0500

    fix

--- public/javascripts/angular/controllers/CoursesController.js
+++ public/javascripts/angular/controllers/CoursesController.js
@@ -1,15 +1,23 @@
 AcademicsModule.controller("CoursesController", ["$scope", "$http", "$routeParams", "$location", function($scope, $http, $routeParams, $location) {
 
+	$scope.idCategory = null;
+
 	$scope.getLanguajes = function() {
-		
+		$http.get("/languajes/get").success(function(languajes) {
+			$scope.languajes = languajes;
+		});
 	};
 
 	$scope.getCategories = function() {
-
+		$http.get("/categories/get").success(function(categories) {
+			$scope.categories = categories;
+		});
 	};
 
 	$scope.getSubCategories = function() {
-		
+		$http.get("/sub_categories/getByCategory/" + $scope.idCategory).success(function(sub_categories) {
+			$scope.sub_categories = sub_categories;
+		});
 	};
 
 }]);
